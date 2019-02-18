aee9554265ae5c9305bd7e9d6a82ff14bf96808c
JM Delgado <delgado@ubuntuserver.geoecology>
2019-02-18 21:31:55 +0100

    a functioning version of the demo

--- app.r
+++ app.r
@@ -1,67 +1,66 @@
 library(shiny)
 library(leaflet)
+library(RPostgreSQL)
+
+
+ui <- fluidPage(leafletOutput("mymap"),p())
+
+
 
 
 server <- function(input, output, session) {
 
-  # points <- eventReactive(input$recalc, {
-  #   cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  # }, ignoreNULL = FALSE)
 
-      output$mymap4 <- renderLeaflet({
-        leaflet() %>%
-          addProviderTiles(providers$CartoDB.Positron) %>%
-          setView(-8,38, zoom=7) %>%
+     output$mymap <- renderLeaflet({
+       leaflet() %>%
+         addProviderTiles(providers$CartoDB.Positron) %>%
+         setView(-8,38, zoom=7) %>%
+         addWMSTiles(
+           "http://141.89.96.184/latestwms",
+            layers = "JRC-Global-Water-Bodies-sib",
+            options = WMSTileOptions(format = "image/png",
+                                     transparent = TRUE,
+                                     version='1.3.0',
+                                     srs='EPSG:4326')) %>%
           addWMSTiles(
-            "http://141.89.96.184/latestwms",
-            #"http://141.89.96.184:8080/geoserver/wms",
+           "http://141.89.96.184/latestwms",
             layers = "watermask-sib",
             options = WMSTileOptions(format = "image/png",
                                      transparent = TRUE,
                                      version='1.3.0',
-                                     srs='EPSG:4326',
-                                     bbox='36,-9,40,-6')) %>%
+                                     srs='EPSG:4326')) %>%
+
           addScaleBar(position = "topleft")
       })
-  #
-  # output$mymap <- renderLeaflet({
-  #   leaflet() %>%
-  #     addProviderTiles(providers$Stamen.TonerLite,
-  #       options = providerTileOptions(noWrap = TRUE)
-  #     ) %>%
-  #     addMarkers(data = points())
-  # })
-}
-      #
-      # # show reservoir information ####
-      # observeEvent(input$mymap4_click, {
-      #   click <- input$mymap4_click
-      #
-      # source("/srv/shiny-server/buhayra-app/pw.R")
-      # drv <- dbDriver("PostgreSQL")
-      # con <- dbConnect(drv, dbname = "watermasks-sib", host = "localhost", port = 5432, user = "sar2water", password = pw)
-      # rm(pw)
-      # # area <- dbGetQuery(con, paste0("SELECT area FROM neb WHERE ST_Contains(geom, ST_SetSRID(ST_Point(",click$lng,",",click$lat,"),4326)) ORDER BY ingestion_time DESC Limit 1"))
-      # area <- dbGetQuery(con, paste0("SELECT area FROM sib WHERE ST_Contains(geom, ST_SetSRID(ST_Point(",click$lng,",",click$lat,"),4326))
-      #                                AND EXTRACT(month FROM ingestion_time) =", input$M3, "AND EXTRACT(year FROM ingestion_time) =", input$Y3, "ORDER BY ingestion_time DESC Limit 1"))
-      #
-      # dbDisconnect(conn = con)
-      #
-      # if(length(area) == 0) {
-      #   text <- "here is no reservoir" #required info
-      #   proxy <- leafletProxy("mymap4")
-      #   proxy %>% clearPopups() %>%
-      #   addPopups(click$lng, click$lat, text)
-      # }
-      #
-      # else {
-      #   text <- paste0("reservoir extent ", input$M3,"-", input$Y3, ": ", round(area/10000, digits = 1), " ha") #required info
-      #   proxy <- leafletProxy("mymap4")
-      #   proxy %>% clearPopups() %>%
-      #   addPopups(click$lng, click$lat, text)
-      # }
 
-      # })
+       # # show reservoir information ####
+       observeEvent(input$mymap_click, {
+         click <- input$mymap_click
+      
+       source("/srv/shiny-server/buhayra-app/pw.R")
+       drv <- dbDriver("PostgreSQL")
+       con <- dbConnect(drv, dbname='watermasks', host = "localhost", port = 5432, user = "sar2water", password = pw)
+       rm(pw)
+       area <- dbGetQuery(con, paste0("SELECT area FROM sib WHERE ST_Contains(geom, ST_SetSRID(ST_Point(",click$lng,",",click$lat,"),4326))
+#                                      AND EXTRACT(month FROM ingestion_time) =", input$M3, "AND EXTRACT(year FROM ingestion_time) =", input$Y3, "ORDER BY ingestion_time DESC Limit 1"))
+      
+       dbDisconnect(conn = con)
+      
+       if(length(area) == 0) {
+         text <- "here is no reservoir" #required info
+         proxy <- leafletProxy("mymap")
+         proxy %>% clearPopups() %>%
+         addPopups(click$lng, click$lat, text)
+       }
+      
+       else {
+         text <- paste0("reservoir extent ", input$M3,"-", input$Y3, ": ", round(area/10000, digits = 1), " ha") #required info
+         proxy <- leafletProxy("mymap")
+         proxy %>% clearPopups() %>%
+         addPopups(click$lng, click$lat, text)
+       }
+
+       })
 
 
 }
