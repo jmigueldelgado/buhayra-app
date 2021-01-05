a454db34e67f42c5a7ca8d1bd09e50891b75fad2
JM Delgado <jmigueldelgado@posteo.net>
2021-01-05 22:33:21 +0100

    test tabpanel

--- ui.r
+++ ui.r
@@ -10,39 +10,73 @@ csscode <- HTML("
 }"
 )
 
-header <- dashboardHeader(
-    title = 'Albufeiras em Tempo Real - Demonstração',
-    titleWidth = 800
-)
+navbarPage("Reservoirs in real time", id="nav",
 
-##### test branch
+  tabPanel("Interactive map",
+    div(class="outer",
 
-body <- dashboardBody(tags$style(csscode),
-  fluidRow(
-    column(width = 9,
-      box(width = NULL, solidHeader = TRUE,
-        leafletOutput("mymap", height = 500)
-      )),
-    column(width = 3,
-      box(width = NULL, status = "warning",
-        checkboxGroupInput("datasets", "Mostrar",
-          choices = c(
-            `Referência (JRC, dados estáticos)` = 1,
-            `buhayra/Sentinel-1` = 2
-          ),
-          selected = c(1,2)
-          ),
-        p(
-            class = "text-muted",
-            paste("Clique sobre uma albufeira para consultar a série temporal")
-        ),
-        plotOutput("plot"))
-)
-)
-)
+      # tags$head(
+      #   # Include our custom CSS
+      #   includeCSS("styles.css"),
+      #   includeScript("gomap.js")
+      # ),
 
-dashboardPage(
-  header,
-  dashboardSidebar(disable = TRUE),
-  body
+      # If not using custom CSS, set height of leafletOutput to a number instead of percent
+      leafletOutput("mymap", width="100%", height="100%"),
+
+      # Shiny versions prior to 0.11 should use class = "modal" instead.
+      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+        width = 330, height = "auto",
+
+        h2("Municipalities"),
+
+        # selectInput("color", "Color", vars),
+        # selectInput("size", "Size", vars, selected = "adultpop"),
+        # conditionalPanel("input.color == 'superzip' || input.size == 'superzip'",
+        #   # Only prompt for threshold when coloring or sizing by superzip
+        #   numericInput("threshold", "SuperZIP threshold (top n percentile)", 5)
+        # ),
+
+        # plotOutput("histCentile", height = 200),
+        plotOutput("plot", height = 250)
+      ),
+
+      # tags$div(id="cite",
+      #   'Data compiled for ', tags$em('Coming Apart: The State of White America, 1960–2010'), ' by Charles Murray (Crown Forum, 2012).'
+      # )
+    )
+  ),
+
+
+  conditionalPanel("false", icon("crosshair"))
 )
+#
+#   fluidRow(
+#     column(width = 9,
+#       box(width = NULL, solidHeader = TRUE,
+#         leafletOutput("mymap", height = 500)
+#       )),
+#     column(width = 3,
+#       box(width = NULL, status = "warning",
+#         checkboxGroupInput("datasets", "Mostrar",
+#           choices = c(
+#             `Referência (JRC, dados estáticos)` = 1,
+#             `buhayra/Sentinel-1` = 2
+#           ),
+#           selected = c(1,2)
+#           ),
+#         p(
+#             class = "text-muted",
+#             paste("Clique sobre uma albufeira para consultar a série temporal")
+#         ),
+#         plotOutput("plot"))
+# )
+# )
+# )
+#
+# dashboardPage(
+#   header,
+#   dashboardSidebar(disable = TRUE),
+#   body
+# )
