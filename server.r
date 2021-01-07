2359917061c7e4eb0caaf537a858ed6b78c24f2a
jmigueldelgado <jmigueldelgado@posteo.net>
2021-01-07 17:42:46 +0100

    adds municipios with population

--- server.r
+++ server.r
@@ -18,12 +18,11 @@ function(input, output, session) {
     wms_layers <- list('Watermasks and Static Water Bodies'=c("watermask","JRC-Global-Water-Bodies"),'Only Watermasks'=c('watermask'),'Aggregate on Municipalities'=c("watermask","JRC-Global-Water-Bodies"))
 
     source("/srv/shiny-server/buhayra-app/pw.R")
-    geojson = jsonlite::fromJSON('data/municipios_ce_simple.geojson')
     
     output$mymap <- renderLeaflet({
         active_layers  <- wms_layers[[input$datasets]]
 
-        map = leaflet(municipios) %>%
+        map=leaflet(municipios) %>%
             addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
             setView(-38.5,-5.3, zoom=12) %>%
             addWMSTiles(paste0("http://",mapserver_host,"/latestwms"),
@@ -33,66 +32,82 @@ function(input, output, session) {
                                                  version='1.3.0',
                                                  srs='EPSG:4326')) %>%
             addScaleBar(position = "topleft")
-
-        if(input$datasets == names(wms_layers)[3] {
         
+        if(input$datasets == names(wms_layers)[3]) {
+            labels <- sprintf(
+                "<strong>%s</strong><br/>%g people",
+                municipios$NM_MUNICIP, municipios$pop) %>%
+                lapply(htmltools::HTML)
+            
             map %>% addPolygons(fillColor = ~pal(pop),
                                 weight = 2,
                                 opacity = 1,
                                 color = "white",
                                 dashArray = "3",
-                                fillOpacity = 0.7)
-        } else {map}
+                                fillOpacity = 0.7,
+                                highlight = highlightOptions(
+                                    weight = 5,
+                                    color = "#666",
+                                    dashArray = "",
+                                    fillOpacity = 0.7,
+                                    bringToFront = TRUE),
+                                label = labels,
+                                labelOptions = labelOptions(
+                                    style = list("font-weight" = "normal", padding = "3px 8px"),
+                                    textsize = "15px",
+                                    direction = "auto"))
+            } else {map}
     })
-
 
 
         observeEvent(input$mymap_click,
         {
-            click <- input$mymap_click
-            source("/srv/shiny-server/buhayra-app/pw.R")
-            drv <- dbDriver("PostgreSQL")
-            con <- dbConnect(drv, dbname='watermasks', host = db_host, port = 5432, user = "sar2water", password = pw)
-            rm(pw)
-            ts <- dbGetQuery(con, paste0("SELECT jrc_demo.id_jrc, ST_area(ST_Transform(jrc_demo.geom,32629)) as ref_area,demo.area,demo.ingestion_time,demo.source_id,scene_demo.mission_id,scene_demo.pass FROM jrc_demo RIGHT JOIN demo ON jrc_demo.id_jrc=demo.id_jrc RIGHT JOIN scene_demo ON demo.ingestion_time = scene_demo.ingestion_time WHERE ST_Contains(jrc_demo.geom, ST_SetSRID(ST_Point(",click$lng,",",click$lat,"),4326))"))
-            dbDisconnect(conn = con)
-
-            if(nrow(ts) == 0)
-            {
-                text <- "Unable to find a reservoir on this location" #required info
-                leafletProxy("mymap") %>%
-                    clearPopups() %>%
-                    addPopups(click$lng, click$lat, text)
-            }
-            else
-            {
-                output$tsVol <- renderPlot({
-                    ts %>%
-                        filter(area>0) %>%
-                        ggplot +
-                        geom_point(aes(x=ingestion_time,y=area/10000,color=mission_id,shape=pass)) +
-                        scale_y_continuous(limits=c(0,1.1*max(ts$ref_area)/10000)) +
-                        geom_hline(yintercept=ts$ref_area[1]/10000,linetype='dashed',color='orange') +
-                        xlab("Acquisition Date") +
-                        ylab("Area [ha]") +
-                        theme(legend.position='bottom')
-                })
+            if(input$datasets != names(wms_layers)[3]) {
+                click <- input$mymap_click
+                source("/srv/shiny-server/buhayra-app/pw.R")
+                drv <- dbDriver("PostgreSQL")
+                con <- dbConnect(drv, dbname='watermasks', host = db_host, port = 5432, user = "sar2water", password = pw)
+                rm(pw)
+                ts <- dbGetQuery(con, paste0("SELECT jrc_demo.id_jrc, ST_area(ST_Transform(jrc_demo.geom,32629)) as ref_area,demo.area,demo.ingestion_time,demo.source_id,scene_demo.mission_id,scene_demo.pass FROM jrc_demo RIGHT JOIN demo ON jrc_demo.id_jrc=demo.id_jrc RIGHT JOIN scene_demo ON demo.ingestion_time = scene_demo.ingestion_time WHERE ST_Contains(jrc_demo.geom, ST_SetSRID(ST_Point(",click$lng,",",click$lat,"),4326))"))
+                dbDisconnect(conn = con)
                 
-                text= paste0("ID: ",ts$id_jrc[1],"<br>",ts %>%
-                                                        mutate(`area [ha]`=area/10000) %>%
-                                                        dplyr::select(`area [ha]`,ingestion_time) %>%
-                                                        distinct(ingestion_time,.keep_all=TRUE) %>%
-                                                        arrange(desc(ingestion_time)) %>%
-                                                        slice(1:3) %>%
-                                                        knitr::kable(.,"html") %>%
-                                                        kableExtra::kable_styling())
-                leafletProxy("mymap") %>%
-                    clearPopups() %>%
-                    addPopups(click$lng, click$lat, text)
+                if(nrow(ts) == 0)
+                {
+                    text <- "Unable to find a reservoir on this location" #required info
+                    leafletProxy("mymap") %>%
+                        clearPopups() %>%
+                        addPopups(click$lng, click$lat, text)
+                }
+                else
+                {
+                    output$tsVol <- renderPlot({
+                        ts %>%
+                            filter(area>0) %>%
+                            ggplot +
+                            geom_point(aes(x=ingestion_time,y=area/10000,color=mission_id,shape=pass)) +
+                            scale_y_continuous(limits=c(0,1.1*max(ts$ref_area)/10000)) +
+                            geom_hline(yintercept=ts$ref_area[1]/10000,linetype='dashed',color='orange') +
+                            xlab("Acquisition Date") +
+                            ylab("Area [ha]") +
+                            theme(legend.position='bottom')
+                    })
+                    
+                    text= paste0("ID: ",ts$id_jrc[1],"<br>",ts %>%
+                                                            mutate(`area [ha]`=area/10000) %>%
+                                                            dplyr::select(`area [ha]`,ingestion_time) %>%
+                                                            distinct(ingestion_time,.keep_all=TRUE) %>%
+                                                            arrange(desc(ingestion_time)) %>%
+                                                            slice(1:3) %>%
+                                                            knitr::kable(.,"html") %>%
+                                                            kableExtra::kable_styling())
+                    leafletProxy("mymap") %>%
+                        clearPopups() %>%
+                        addPopups(click$lng, click$lat, text)
+                }
             }
-        })
-    
+            })
+        
     output$selected_var <- renderText({
-        wms_layers[[input$datasets]]
+        input$datasets == names(wms_layers)[3]
     })
 }
--- ui.r
+++ ui.r
@@ -33,37 +33,3 @@ navbarPage(
 )
 
 
-    
-#ui <- fluidPage(leafletOutput("mymap"),p(), actionButton("recalc", "New points"))
-
-
-
-#
-#   fluidRow(
-#     column(width = 9,
-#       box(width = NULL, solidHeader = TRUE,
-#         leafletOutput("mymap", height = 500)
-#       )),
-#     column(width = 3,
-#       box(width = NULL, status = "warning",
-#         checkboxGroupInput("datasets", "Mostrar",
-#           choices = c(
-#             `Referência (JRC, dados estáticos)` = 1,
-#             `buhayra/Sentinel-1` = 2
-#           ),
-#           selected = c(1,2)
-#           ),
-#         p(
-#             class = "text-muted",
-#             paste("Clique sobre uma albufeira para consultar a série temporal")
-#         ),
-#         plotOutput("plot"))
-# )
-# )
-# )
-#
-# dashboardPage(
-#   header,
-#   dashboardSidebar(disable = TRUE),
-#   body
-# )
