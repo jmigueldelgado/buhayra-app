176671b435f6f9ea085f9a64a1a42112853d0bbf
JM Delgado <jmigueldelgado@posteo.net>
2021-01-12 14:46:48 +0100

    adds ST_Perimeter to query

--- R/utilsServer.R
+++ R/utilsServer.R
@@ -9,7 +9,7 @@ query_watermask  <- function(click){
     drv <- dbDriver("PostgreSQL")
     con <- dbConnect(drv, dbname='watermasks', host = db_host, port = 5432, user = "sar2water", password = pw)
     rm(pw)
-    ts <- dbGetQuery(con, paste0("SELECT jrc_demo.id_jrc, ST_area(ST_Transform(jrc_demo.geom,32629)) as ref_area,demo.area,demo.ingestion_time,demo.source_id,scene_demo.mission_id,scene_demo.pass FROM jrc_demo RIGHT JOIN demo ON jrc_demo.id_jrc=demo.id_jrc RIGHT JOIN scene_demo ON demo.ingestion_time = scene_demo.ingestion_time WHERE ST_Contains(jrc_demo.geom, ST_SetSRID(ST_Point(",click$lng,",",click$lat,"),4326))"))
+    ts <- dbGetQuery(con, paste0("SELECT jrc_demo.id_jrc, ST_area(ST_Transform(jrc_demo.geom,32629)) as ref_area, ST_Perimeter(ST_Transform(jrc_demo.geom,32629)) as ref_perimeter, demo.area,demo.ingestion_time,demo.source_id,scene_demo.mission_id,scene_demo.pass FROM jrc_demo RIGHT JOIN demo ON jrc_demo.id_jrc=demo.id_jrc RIGHT JOIN scene_demo ON demo.ingestion_time = scene_demo.ingestion_time WHERE ST_Contains(jrc_demo.geom, ST_SetSRID(ST_Point(",click$lng,",",click$lat,"),4326))"))
     dbDisconnect(conn = con)
     return(ts)
 }
@@ -26,7 +26,7 @@ query_on_sf  <- function(pol){
     con <- dbConnect(drv, dbname='watermasks', host = db_host, port = 5432, user = "sar2water", password = pw)
     rm(pw)
 
-    q_string=paste0("SELECT jrc_demo.id_jrc, ST_area(ST_Transform(jrc_demo.geom,32629)) as ref_area,demo.area,demo.ingestion_time,demo.source_id FROM jrc_demo RIGHT JOIN demo ON jrc_demo.id_jrc=demo.id_jrc WHERE ST_Intersects(jrc_demo.geom, '",st_as_text(pol$geometry,EWKT=TRUE),"');")
+    q_string=paste0("SELECT jrc_demo.id_jrc, ST_area(ST_Transform(jrc_demo.geom,32629)) as ref_area,, ST_Perimeter(ST_Transform(jrc_demo.geom,32629)) as ref_perimeter, demo.area,demo.ingestion_time,demo.source_id FROM jrc_demo RIGHT JOIN demo ON jrc_demo.id_jrc=demo.id_jrc WHERE ST_Intersects(jrc_demo.geom, '",st_as_text(pol$geometry,EWKT=TRUE),"');")
 
     ts <- dbGetQuery(con,q_string)
     dbDisconnect(conn = con)
@@ -57,7 +57,7 @@ plot_aggregated_ts  <- function(ts) {
         ungroup %>%
         group_by(ingestion_time) %>%
         summarise(`Acquisition Date`=first(ingestion_time),`Total Area [ha]`=sum(area),`Reference Area`=sum(ref_area),Mission=first(source_id))
-    
+
     plt = ts_crunched %>%
         ggplot +
         geom_point(aes(x=`Acquisition Date`,y=`Total Area [ha]`/10000,color=Mission)) +
